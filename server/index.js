2ddb6e91f3c60239ac9c85ac0f21e4dd7aced979
Ethan Miles <ethanmilescs@gmail.com>
2022-07-14 11:28:14 -0500

    Added base api route and .env to index.js

--- server/index.js
+++ server/index.js
@@ -1,13 +1,19 @@
 const express = require('express')
+require('dotenv').config()
 const mongoose = require('mongoose')
 const app = express()
 
 const UserModel = require('./Users')
+const PORT = process.env.PORT || 3001
 
 app.use(express.json())
 
 mongoose.connect('mongodb+srv://newuser:QPY5TvVWvcLCMkOT@take-home.506qia4.mongodb.net/app-users?retryWrites=true&w=majority', {
     useNewUrlParser: true
+})
+
+app.use('/', (req,res) => {
+    res.status(200).send('<h1>Welcome to the lowest level of the api</h1>')
 })
 
 app.get('/', async(req, res) => {
@@ -19,6 +25,6 @@ app.get('/', async(req, res) => {
     }
 })
 
-app.listen(3001, () => {
-    console.log('Server running on port 3001')
+app.listen(PORT, () => {
+    console.log(`Server running on port ${PORT}`)
 })
