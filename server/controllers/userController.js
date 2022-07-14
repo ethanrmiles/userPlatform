c4e49ad83189ebb9abfd9b1b99c160f06fb563e2
Ethan Miles <ethanmilescs@gmail.com>
2022-07-14 13:48:02 -0500

    made changes to getUserById()

--- server/controllers/userController.js
+++ server/controllers/userController.js
@@ -11,10 +11,13 @@ const getAllUsers = async(req,res) => {
     }
 }
 
-const getUserById = async (id) => {
-    const userById = await User.findById(id)
-    return userById
-    res.status(200).json({ message: 'This is my user profile'})
+const getUserById = async (req, res) => {
+   try{ 
+    const user = await User.find(req.params.id)
+    res.status(200).json({ user })
+   }catch(err){
+    res.status(404).json({ message: "Sorry, no user was found"})
+   }
 }
 
 const newUser = async (req, res) => {
