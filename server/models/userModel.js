0c024220dd3d4a3336486407394aa90801b6698e
Ethan Miles <ethanmilescs@gmail.com>
2022-07-14 11:57:53 -0500

    Created model functions that will enable route operation

---
+++ server/models/userModel.js
@@ -0,0 +1,22 @@
+const getAllUsers = () => {
+
+}
+
+const getUserById = () => {
+
+}
+
+const insert = () => {
+
+}
+
+const update = () => {
+
+}
+
+module.exports = {
+    getAllUsers, 
+    getUserById,
+    insert,
+    update
+}
