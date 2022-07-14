8575e1ff98eed50576dcf458b6bfde4fd439dc76
Ethan Miles <ethanmilescs@gmail.com>
2022-07-14 13:13:16 -0500

    Added db schema

---
+++ server/controllers/userController.js
@@ -0,0 +1,25 @@
+
+
+
+const getAllUsers = () => {
+
+}
+
+const getUserById = () => {
+
+}
+
+const insert = () => {
+
+}
+
+const update = () => {
+
+}
+
+module.exports = {
+    getAllUsers, 
+    getUserById,
+    insert,
+    update
+}
--- server/models/userModel.js
+++ server/models/userModel.js
@@ -1,22 +1,19 @@
-const getAllUsers = () => {
+const mongoose = require('mongoose')
 
+const userSchema = mongoose.Schema({
+    name: {
+        type: String,
+        required: [true, 'Please add Name']
+    },
+    description: {
+        type: String,
+        required: [true, 'Please add description']
+    },
+    
+}, 
+{
+    timestamps: true
 }
+)
 
-const getUserById = () => {
-
-}
-
-const insert = () => {
-
-}
-
-const update = () => {
-
-}
-
-module.exports = {
-    getAllUsers, 
-    getUserById,
-    insert,
-    update
-}
+module.exports = mongoose.model('Users', userSchema)
