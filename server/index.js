1478417958482172ec859f8c9069ffc0d0834eea
Ethan Miles <ethanmilescs@gmail.com>
2022-07-14 11:59:18 -0500

    Decided to refactor db connection files

--- server/index.js
+++ server/index.js
@@ -10,21 +10,8 @@ const PORT = process.env.PORT || 3001
 app.use(express.json())
 app.use(express.urlencoded({ extended: false }))
 
-mongoose.connect('mongodb+srv://newuser:QPY5TvVWvcLCMkOT@take-home.506qia4.mongodb.net/app-users?retryWrites=true&w=majority', {
-    useNewUrlParser: true
-})
-
 app.use('/api/users', usersRouter)
 app.use('/api/user', userRouter)
-
-app.get('/', async(req, res) => {
-    const user = new UserModel({name: 'Ethan', description: 'foobarbaz'})
-    try{
-        await user.save()
-    }catch(err){
-        console.log(err)
-    }
-})
 
 app.listen(PORT, () => {
     console.log(`Server running on port ${PORT}`)
